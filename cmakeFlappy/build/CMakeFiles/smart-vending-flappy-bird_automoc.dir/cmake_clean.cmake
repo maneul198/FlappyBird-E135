2071b9f83d2eafdcd22ecac400d9043c6c988133
maneul198 <fkrskl@protonmail.ch>
2017-07-14 16:05:56 -0500

    Primera version FlappyBird Cabitech

--- cmakeFlappy/build/CMakeFiles/smart-vending-flappy-bird_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "smart-vending-flappy-bird_automoc.cpp"
-  "CMakeFiles/smart-vending-flappy-bird_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/smart-vending-flappy-bird_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
